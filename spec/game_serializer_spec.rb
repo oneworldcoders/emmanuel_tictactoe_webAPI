f7ad4552ce58ca586e8ea740bfa109350877086c
Emmanuel Omona <emmanuelomona1@gmail.com>
2020-04-23 11:05:32 +0200

    add description to tests

--- spec/game_serializer_spec.rb
+++ spec/game_serializer_spec.rb
@@ -7,14 +7,14 @@ class FakeModel
 end
 
 describe GameSerializer do
-  it '' do
+  it 'serializes the model' do
     game_serializer = GameSerializer.new
     result = game_serializer.serialize(FakeModel.new([], 'X'))
     expected = { state: [], turn: 'X' }
     expect(result).to eq(expected)
   end
 
-  it 'serializes default data' do
+  it 'serializes the model with default data' do
     game_serializer = GameSerializer.new
     result = game_serializer.serialize(nil)
     expected = { state: ['', '', '', '', '', '', '', '', ''], turn: 'X' }
