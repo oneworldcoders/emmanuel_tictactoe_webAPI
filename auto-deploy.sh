903b13649e61d149d30f50482d2357b5e6d49a20
Emmanuel Omona <emmanuelomona1@gmail.com>
2020-06-12 12:01:14 +0200

    add pem file keypair to env

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-chmod 400 tictactoe-ubuntu-keypair.pem
-ssh -i tictactoe-ubuntu-keypair.pem ubuntu@18.222.151.48 "
+ssh-keyscan -H 18.222.151.48 >> ~/.ssh/known_hosts
+
+printenv PRIVATE_KEY >> /tmp/keypair.pem
+chmod 600 /tmp/keypair.pem
+ssh -i /tmp/keypair.pem ubuntu@18.222.151.48 "
   cd emmanuel_tictactoe_webAPI
   git pull origin
   sudo docker-compose down
   sudo docker-compose build
-  sudo docker-compose up"
+  sudo docker-compose up -d"
+
+rm /tmp/keypair.pem
